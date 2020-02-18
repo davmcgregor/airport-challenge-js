35e0f20afeb95dad50ab2f19d0313f6c1096374e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-18 15:30:26 +0000

    Tests pass

--- src/Plane.js
+++ src/Plane.js
@@ -1,7 +1,21 @@
 'use strict';
 
-function Plane() {}
+function Plane(){
+  // this._location;
+  
+//   this.land = function(airport) {
+//     airport.clearForLanding(this);
+//     location = airport;
+//   };
+//   this.takeoff = function() {
+//     location.clearForTakeOff('')
+//   };
+}
 
 Plane.prototype.land = function(airport){
   airport.clearForLanding(this);
+  this._location = airport;
+};
+Plane.prototype.takeoff = function(airport){
+  this._location.clearForTakeOff('');
 };
